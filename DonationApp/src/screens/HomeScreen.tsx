e917ec9bc7f870af4b1d50d40ce9f9915a5d0b6e
musta <loofey14@hotmai.com>
2019-09-19 17:01:30 +0100

    add more projects for testing

--- DonationApp/src/screens/HomeScreen.tsx
+++ DonationApp/src/screens/HomeScreen.tsx
@@ -32,21 +32,58 @@ const projects = [
   },
   {
     id: 1, title: 'Make new hospital',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, nobis, possimus? Dolor ipsam repudiandae veritatis?', total: 500,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, nobis, possimus? Dolor ipsam repudiandae veritatis?',
+    total: 500,
     done: 370,
   },
   {
-    id: 2, title: 'Make new hospital', description: 'Lorem ipsum dolor sit amet.', total: 500,
+    id: 2, title: 'Make new hospital',
+    description: 'Lorem ipsum dolor sit amet.', total: 500,
     done: 30,
   },
   {
-    id: 3, title: 'Make new hospital', description: 'Lorem ipsum dolor sit amet.', total: 500,
+    id: 3, title: 'Make new hospital',
+    description: 'Lorem ipsum dolor sit amet.',
+    total: 500,
     done: 177,
   },
   {
-    id: 4, title: 'Make new hospital', description: 'Lorem ipsum dolor sit amet.', total: 500,
+    id: 4, title: 'Make new hospital',
+    description: 'Lorem ipsum dolor sit amet.',
+    total: 500,
     done: 400,
   },
+  {
+    id: 5, title: 'Make new hospital',
+    description: 'للمهتمين قمنا بوضع نص لوريم إبسوم القياسي والمُستخدم منذ القرن الخامس عشر في الأسفل. وتم أيضاً توفير الأقسام 1.10.32 و 1.10.33 من \"حول أقاصي الخير والشر\" (de Finibus Bonorum et Malorum) لمؤلفه شيشيرون (Cicero) بصيغها الأصلية، مرفقة بالنسخ الإنكليزية لها والتي قام بترجمتها هـ.راكهام (H. Rackham) في عام 1914',
+    total: 700,
+    done: 400,
+  },
+  {
+    id: 6, title: 'Make new hospital',
+    description: 'للمهتمين قمنا بوضع نص لوريم إبسوم القياسي والمُستخدم منذ القرن الخامس عشر في الأسفل. وتم أيضاً توفير الأقسام 1.10.32 و 1.10.33 من \"حول أقاصي الخير والشر\" (de Finibus Bonorum et Malorum) لمؤلفه شيشيرون (Cicero) بصيغها الأصلية، مرفقة بالنسخ الإنكليزية لها والتي قام بترجمتها هـ.راكهام (H. Rackham) في عام 1914',
+    total: 150,
+    done: 90,
+  },
+  {
+    id: 7, title: 'Make new hospital',
+    description: 'Lorem ipsum dolor sit amet.',
+    total: 800,
+    done: 764,
+  },
+  {
+    id: 8, title: 'Make new hospital',
+    description: 'Lorem ipsum dolor sit amet.',
+    total: 500,
+    done: 356,
+  },
+  {
+    id: 9, title: 'Make new hospital',
+    description: 'Lorem ipsum dolor sit amet.',
+    total: 500,
+    done: 321,
+  },
+
 ];
 
 class HomeScreen extends Component<Props, State> {
