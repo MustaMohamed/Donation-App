ada86d96ab985cc5e61ad8b3ee0419192af4af4a
musta <loofey14@hotmai.com>
2019-09-22 17:36:18 +0100

    change donor personal data to optional switch

--- DonationApp/src/screens/DonationFormScreen.tsx
+++ DonationApp/src/screens/DonationFormScreen.tsx
@@ -3,9 +3,9 @@
  */
 
 import React, { PureComponent } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Button, Icon, Input } from 'react-native-elements';
-import { injectIntl, IntlShape } from 'react-intl';
+import { StyleSheet, Switch, Text, View } from 'react-native';
+import { Button, Icon, Input, ListItem } from 'react-native-elements';
+import { FormattedMessage, injectIntl, IntlShape } from 'react-intl';
 import { NavigationStackProp } from 'react-navigation-stack';
 import { NavigationParams, NavigationState } from 'react-navigation';
 import { navigationConstants, translationConstants } from '../constants';
@@ -15,7 +15,15 @@ interface Props {
   navigation: NavigationStackProp<NavigationState, NavigationParams>;
 }
 
+interface State {
+  donorDetailsIsVisible: boolean;
+}
+
 class DonationFormScreen extends PureComponent<Props> {
+  state = {
+    donorDetailsIsVisible: false,
+  };
+
   static navigationOptions = ({ screenProps, navigation }) => {
     const project = navigation.getParam(navigationConstants.SCREEN_PARAM_PROJECT);
     const title: string = screenProps.intl.formatMessage({ id: translationConstants.DONATE });
@@ -24,54 +32,89 @@ class DonationFormScreen extends PureComponent<Props> {
     };
   };
 
+  _onDonorDetailsIsVisibleChange = (value) => {
+    this.setState({
+      donorDetailsIsVisible: value,
+    });
+  };
+
   render() {
     return (
-      <View>
-        <Input
-          inputStyle={styles.input}
-          placeholder={this.props.intl.formatMessage({ id: translationConstants.DONOR_NAME_PLACEHOLDER })}
-          rightIcon={
-            <Icon color={'gray'}
-                  name={'account-card-details'}
-                  type={'material-community'}
-                  size={22}/>
-          }
-        />
-        <Input
-          inputStyle={styles.input}
-          placeholder={this.props.intl.formatMessage({ id: translationConstants.DONOR_PHONE_PLACEHOLDER })}
-          rightIcon={
-            <Icon color={'gray'}
-                  name={'whatsapp'}
-                  type={'material-community'}
-                  size={22}/>
-          }
-        />
+      <View style={styles.container}>
+        <View style={styles.switchView}>
+          <ListItem
+            rightElement={<Switch style={styles.switch} value={this.state.donorDetailsIsVisible} thumbColor={'#40A9FF'} trackColor={{ true: '#a0c1ff', false: 'gray' }}
+                                  onValueChange={this._onDonorDetailsIsVisibleChange}/>}
+            title={<Text style={styles.text}><FormattedMessage id={translationConstants.SWITCH_DONOR_PERSONAL_DETAILS}/></Text>}
 
-        <Input
-          inputStyle={styles.input}
-          placeholder={this.props.intl.formatMessage({ id: translationConstants.DONOR_MONEY_AMOUNT })}
-          rightIcon={
-            <Icon color={'gray'}
-                  name={'money'}
-                  type={'font-awesome'}
-                  size={22}/>
-          }
-        />
-        <Button buttonStyle={styles.donateBtn} title={this.props.intl.formatMessage({ id: translationConstants.DONATE })}/>
+          />
+          {/* If you want to list your name to this project donation list switch this button*/}
+
+        </View>
+        <View style={styles.actionsView}>
+          {this.state.donorDetailsIsVisible && <View>
+            <Input
+              inputStyle={styles.input}
+              placeholder={this.props.intl.formatMessage({ id: translationConstants.DONOR_NAME_PLACEHOLDER })}
+              rightIcon={
+                <Icon color={'gray'}
+                      name={'account-card-details'}
+                      type={'material-community'}
+                      size={22}/>
+              }
+            />
+            <Input
+              inputStyle={styles.input}
+              placeholder={this.props.intl.formatMessage({ id: translationConstants.DONOR_PHONE_PLACEHOLDER })}
+              rightIcon={
+                <Icon color={'gray'}
+                      name={'whatsapp'}
+                      type={'material-community'}
+                      size={22}/>
+              }
+            />
+          </View>}
+          <Input
+            inputStyle={styles.input}
+            placeholder={this.props.intl.formatMessage({ id: translationConstants.DONOR_MONEY_AMOUNT })}
+            rightIcon={
+              <Icon color={'gray'}
+                    name={'money'}
+                    type={'font-awesome'}
+                    size={22}/>
+            }
+          />
+          <Button buttonStyle={styles.donateBtn} title={this.props.intl.formatMessage({ id: translationConstants.DONATE })}/>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 5,
+  },
   donateBtn: {
     marginVertical: 10,
     marginHorizontal: 10,
   },
+  text: {
+    textAlign: 'left',
+  },
   input: {
     marginHorizontal: 10,
   },
+  actionsView: {
+    marginTop: 20,
+    flexDirection: 'column',
+  },
+  switchView: {
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+  },
+  switch: {},
 });
 
 export default injectIntl(DonationFormScreen);
