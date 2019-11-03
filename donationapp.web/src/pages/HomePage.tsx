52b70e6772c1ac9637902d57c4bd22d412f9bf0f
musta <loofey14@hotmai.com>
2019-11-03 17:26:41 +0200

    fix home second navbar

--- donationapp.web/src/pages/HomePage.tsx
+++ donationapp.web/src/pages/HomePage.tsx
@@ -78,30 +78,7 @@ class DesktopContainer extends Component<any, any> {
             style={{ minHeight: 700, padding: '1em 0em' }}
             vertical
           >
-            <Menu
-              fixed={fixed ? 'top' : null}
-              inverted={!fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size='large'
-            >
-              <Container>
-                <Menu.Item as='a' active>
-                  Home
-                </Menu.Item>
-                <Menu.Item as='a'>Work</Menu.Item>
-                <Menu.Item as='a'>Company</Menu.Item>
-                <Menu.Item as='a'>Careers</Menu.Item>
-                <Menu.Item position='right'>
-                  <Button as='a' inverted={!fixed}>
-                    Log in
-                  </Button>
-                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
-                    Sign Up
-                  </Button>
-                </Menu.Item>
-              </Container>
-            </Menu>
+
             <HomepageHeading mobile={fixed}/>
           </Segment>
         </Visibility>
@@ -153,21 +130,6 @@ class MobileContainer extends Component<any, any> {
             style={{ minHeight: 350, padding: '1em 0em' }}
             vertical
           >
-            <Container>
-              <Menu inverted pointing secondary size='large'>
-                <Menu.Item onClick={this.handleToggle}>
-                  <Icon name='sidebar'/>
-                </Menu.Item>
-                <Menu.Item position='right'>
-                  <Button as='a' inverted>
-                    Log in
-                  </Button>
-                  <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-                    Sign Up
-                  </Button>
-                </Menu.Item>
-              </Menu>
-            </Container>
             <HomepageHeading mobile/>
           </Segment>
 
@@ -185,7 +147,6 @@ const ResponsiveContainer = ({ children }) => (
     <MobileContainer>{children}</MobileContainer>
   </div>
 );
-
 
 
 const HomepageLayout = () => (
